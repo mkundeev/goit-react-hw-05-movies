772eae630094de3fe63888e45dc5b7db8bae812f
Maksym Kundeev <mkundeev@gmail.com>
2022-07-06 17:59:48 +0300

    add spiner and code spliting

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,52 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import Home from 'pages/Home';
-import Movies from 'pages/Movies';
-import Navigation from './Navigation';
-import SingleMovieInfo from 'pages/SingleMovieInfo';
-import MovieCast from './MovieCast';
-import MovieReviews from './MovieReviews';
+import { Loader } from 'pages/page.styled';
+import { BallTriangle } from 'react-loader-spinner';
+
+const Navigation = lazy(() =>
+  import('./Navigation' /* webpackChunkName: "navigation" */)
+);
+const Home = lazy(() => import('pages/Home' /* webpackChunkName: "home" */));
+const Movies = lazy(() =>
+  import('pages/Movies' /* webpackChunkName: "movies" */)
+);
+const SingleMovieInfo = lazy(() =>
+  import('pages/SingleMovieInfo' /* webpackChunkName: "single-movie" */)
+);
+const MovieCast = lazy(() =>
+  import('./MovieCast' /* webpackChunkName: "movie-cast" */)
+);
+const MovieReviews = lazy(() =>
+  import('./MovieReviews' /* webpackChunkName: "movie-reviews" */)
+);
 
 export const App = () => {
   return (
     <div>
-      <Navigation />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:movieId/*" element={<SingleMovieInfo />}>
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <Loader>
+            <BallTriangle
+              height="200px"
+              width="200px"
+              color="grey"
+              ariaLabel="loading-indicator"
+            />
+          </Loader>
+        }
+      >
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:movieId/*" element={<SingleMovieInfo />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
       <ToastContainer autoClose={2000} />
     </div>
   );
--- src/pages/page.styled.js
+++ src/pages/page.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { BallTriangle } from 'react-loader-spinner';
 
 export const SectionWrap = styled.div`
   padding: 20px;
@@ -6,4 +7,9 @@ export const SectionWrap = styled.div`
 export const Warning = styled.p`
   margin-bottom: 20px;
   font-weight: 500;
+`;
+export const Loader = styled.div`
+  position: fixed;
+  left: 200px;
+  top: 200px;
 `;
